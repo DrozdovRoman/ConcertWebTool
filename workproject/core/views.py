5f52fc1176b3b2fd14ffddf404ced43bb3bc45aa
a1ir <romandrozdov@icloud.com>
2022-10-26 12:52:43 +0500

    Create main page

--- workproject/core/views.py
+++ workproject/core/views.py
@@ -1,3 +1,12 @@
+from re import T
 from django.shortcuts import render
 
 # Create your views here.
+
+def home(request):
+    context = {
+
+    }
+    
+    template = "core/index.html"
+    return render(request,template,context)
